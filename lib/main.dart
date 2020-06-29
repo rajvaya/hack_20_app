69c32db3aad6e0f31d9870519ac829f50e8b84db
raj vaya <rajkumarvaya@gmail.com>
2020-06-29 12:36:25 +0530

    icon Updates

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Empower',
       theme: ThemeData(
         applyElevationOverlayColor: true,
         // This is the theme of your application.
